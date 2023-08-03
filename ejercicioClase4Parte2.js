e483d092a2ac02549b7dc9015de92a00e9623606
ClaudiaLorenaN <claudia.2210.cn@gmail.com>
2023-08-02 21:10:51 -0500

    ejercicio2 clase 4 grupo 1 commit1

---
+++ ejercicioClase4Parte2.js
@@ -0,0 +1,18 @@
+//ir a concierto
+tipoMusica = "salsa";
+temperatura = 20;
+estaLloviendo = true;
+tengoElTicket = true;
+horaActual = 20;
+horaDelConcierto = 21;
+valorTicket = 100;
+
+
+//compra libro
+nombreLibro = "tus zonas erroneas";
+editorial = "penguin";
+autor = "Wayne W. Dyer"
+tipoLibro = "autoayuda";
+numeroPaginas = 312;
+isbn = 12266;
+valor = 1200;
